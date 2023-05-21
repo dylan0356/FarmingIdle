382a3a97aae26a00500bcc250c454880029f0cfa
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-05-21 16:51:17 -0600

    Star UI
    Added UI for the collection of stars. When player runs over a star, it will now update the "Stars Collected: " UI by 1 every time.

--- Assets/ItemCollector.cs
+++ Assets/ItemCollector.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
     public int numStars = 0;
+    [SerializeField] private TextMeshProUGUI starText;
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.CompareTag("Star")) {
             Destroy(col.gameObject);
             numStars++;
-            Debug.Log("Stars: " + numStars);
+            starText.text = "Stars Collected: " + numStars;
         }
     }
 }
