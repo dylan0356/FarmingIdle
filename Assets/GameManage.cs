e29bd9ec267347ce46e32d4a6ef4f2de11eba96a
Dylan <33008329+dylan0356@users.noreply.github.com>
2023-05-23 10:44:27 -0700

    Fixed Death screen

--- Assets/GameManage.cs
+++ Assets/GameManage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManage : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class GameManage : MonoBehaviour
 
     [SerializeField] private GameObject levelLight;
 
+    [SerializeField] private TextMeshProUGUI starsText;
+    [SerializeField] private TextMeshProUGUI percentageCompletionText;
+    [SerializeField] private TextMeshProUGUI percentageCompletionTextMainCanvas;
+    private GameObject player;
+
 
 
 
@@ -20,6 +26,12 @@ public class GameManage : MonoBehaviour
 
         //set the lastPlayerY to the absolute value of the player's y position
         lastPlayerY = Mathf.Abs(GameObject.FindGameObjectWithTag("Player").transform.position.y);
+
+        //set stars on GameOverCanvas to the stars from the Player item collector script
+        starsText.text = "Stars: " + player.GetComponent<ItemCollector>().numStars.ToString() + "/3";
+
+        //set text Completion on GameOverCanvas to PercentageCompletion text from main cavas
+        percentageCompletionText.text = "Completion: " + percentageCompletionTextMainCanvas.text;
 
         //set the player to inactive
         GameObject.FindGameObjectWithTag("Player").SetActive(false);
@@ -34,6 +46,9 @@ public class GameManage : MonoBehaviour
         //find gameobject LevelLight
         levelLight = GameObject.FindGameObjectWithTag("LevelLight");
 
+        //find gameobject Player
+        player = GameObject.FindGameObjectWithTag("Player");
+
 
     }
 
@@ -45,11 +60,12 @@ public class GameManage : MonoBehaviour
         currentPlayerY = 0.5f - (currentPlayerY / 500);
 
         //change the LevelLight objects m_Intensity to the lastPlayerY
-        levelLight.GetComponent<UnityEngine.Rendering.Universal.Light2D>().intensity = currentPlayerY;
+        levelLight.GetComponent<UnityEngine.Rendering.Universal.Light2D>().intensity = currentPlayerY; 
+    }
 
-        
-        
-        
+    public void RestartLevel() {
+        //reload the current scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 
 }
